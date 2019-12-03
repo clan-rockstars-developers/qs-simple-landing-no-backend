cbfe9bd858af27a687dff09cb48082738bfdd4db
Menonro <nitayd2001@gmail.com>
2019-12-03 20:19:42 +0600

    0.0.2 #2

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
 
 const html = require('./webpack/html')
 
 const isProduction = process.env.NODE_ENV === 'production'
 
-console.log('production', isProduction)
+console.log(`Production: ${isProduction}`)
 
 module.exports = {
   output: {
@@ -16,9 +18,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        use: [ "babel-loader" ]
       },{
         test: /\.pug$/,
         use: [
@@ -28,20 +28,15 @@ module.exports = {
       },{
         test: /\.sass$/,
         use: [
-          // isProduction ? MiniCssExtractPlugin.loader : "style-loader",
           MiniCssExtractPlugin.loader,
           {
             loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
+            options: { importLoaders: 1 }
           },
           {
             loader: 'postcss-loader',
             options: {
-              config: {
-                path: 'webpack/'
-              }
+              config: { path: 'webpack/' }
             }
           },
           "sass-loader"
@@ -49,13 +44,23 @@ module.exports = {
       }
     ]
   },
+  optimization: {
+    minimizer: isProduction ? [new TerserJSPlugin({
+      terserOptions: {
+        ecma: 3,
+        ie8: true
+      }
+    })] : []
+  },
+  performance: {
+    hints: isProduction ? "warning" : false
+  },
   plugins: [
     ...html,
     new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
       filename: "[name].css",
       chunkFilename: "chunk-[id].css"
-    })
+    }),
+    new HardSourceWebpackPlugin()
   ]
 };
